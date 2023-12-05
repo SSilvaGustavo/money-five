409fe04e2c08c4efa58cd406c74b0931c05a7d8c
Gustavo Souza <gustavo.s.silva651@gmail.com>
2023-12-05 18:14:35 -0300

    style: Changing footer to relative

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -7,37 +7,37 @@ import { FaEnvelope, FaPhone, FaLocationDot } from "react-icons/fa6";
 
 export default function Footer() {
   return (
-    <footer className="w-full flex flex-col gap-16 pt-8 pb-4 px-32 mt-8 bg-secondary-300">
+    <footer className="relative bottom-0 w-full flex flex-col gap-6 pt-8 pb-4 px-32 mt-8 bg-secondary-300">
       <div className="flex w-full justify-between cursor-pointer">
         <div className="flex flex-col gap-8">
           <TextStyle.Title title="Contato"/>
           <div className="flex items-center gap-2 text-white">
             <FaPhone/>
-            <TextStyle.Paragraph className="hover:text-white transition-colors">(11) 99999-9999</TextStyle.Paragraph>
+            <TextStyle.Paragraph className="hover:text-white transition-colors text-sm">(11) 99999-9999</TextStyle.Paragraph>
           </div>
           <div className="flex items-center gap-2 text-white">
           <FaEnvelope />
-          <TextStyle.Paragraph className="hover:text-white transition-colors">Invest@AtlasLPS.com</TextStyle.Paragraph>
+          <TextStyle.Paragraph className="hover:text-white transition-colors text-sm">Contato@Dashsphere.com</TextStyle.Paragraph>
           </div>
           <div className="flex items-center gap-2 text-white">
           <FaLocationDot />
-          <TextStyle.Paragraph className="hover:text-white transition-colors">
+          <TextStyle.Paragraph className="hover:text-white transition-colors text-sm">
             501 West Broadway, Suite 800, San Diego, CA 92101
           </TextStyle.Paragraph>
           </div>
         </div>
         <div className="flex flex-col gap-3">
-          <TextStyle.Title title="Links" />
-          <TextStyle.Paragraph className="hover:text-white transition-colors">FAQ's</TextStyle.Paragraph>
-          <TextStyle.Paragraph className="hover:text-white transition-colors">Divulgações</TextStyle.Paragraph>
-          <TextStyle.Paragraph className="hover:text-white transition-colors">Política de privacidade</TextStyle.Paragraph>
-          <TextStyle.Paragraph className="hover:text-white transition-colors">Mídias</TextStyle.Paragraph>
-          <TextStyle.Paragraph className="hover:text-white transition-colors">Atualizações</TextStyle.Paragraph>
+          <TextStyle.Title title="Links"/>
+          <TextStyle.Paragraph className="hover:text-white transition-colors text-sm">FAQ's</TextStyle.Paragraph>
+          <TextStyle.Paragraph className="hover:text-white transition-colors text-sm">Divulgações</TextStyle.Paragraph>
+          <TextStyle.Paragraph className="hover:text-white transition-colors text-sm">Política de privacidade</TextStyle.Paragraph>
+          <TextStyle.Paragraph className="hover:text-white transition-colors text-sm">Mídias</TextStyle.Paragraph>
+          <TextStyle.Paragraph className="hover:text-white transition-colors text-sm">Atualizações</TextStyle.Paragraph>
         </div>
         <div className="flex flex-col w-[30%] gap-2">
           <TextStyle.Title title="Termos de serviço" />
-          <TextStyle.Paragraph>
-            A Money Five fornece informações e análises relacionadas a
+          <TextStyle.Paragraph className="text-sm">
+            A Dash Sphere fornece informações e análises relacionadas a
             investimentos e notícias financeiras com o objetivo de educar e
             informar nossos leitores. No entanto, é crucial observar que todas
             as informações contidas neste site são fornecidas apenas para fins
@@ -47,9 +47,9 @@ export default function Footer() {
         </div>
       </div>
       <div className="flex gap-8 justify-center items-center text-white">
-        <Image src={logo} alt="" className="w-36" />
-        <p className="font-normal">
-          MONEY FIVE 2023 @ Todos os direitos reservados
+        <Image src={logo} alt="" className="w-28" />
+        <p className="font-medium">
+          DASH SPHERE 2023 @ Todos os direitos reservados
         </p>
       </div>
     </footer>
